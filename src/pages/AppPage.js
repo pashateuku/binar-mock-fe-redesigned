38fa9cd572b29a43eacfda6cfcd54c67e1403122
Teuku Pasha <thariqteuku.soc@gmail.com>
2022-10-27 00:14:20 +0700

    add: overflow scroll on task list & striketrough when checked

--- src/pages/AppPage.js
+++ src/pages/AppPage.js
@@ -33,15 +33,91 @@ function App() {
                         />
                         <input type='image' className='image-button' src={addButtonImg} alt=''/>
                     </form>
-                    <div className='app-white__task'>
-                        <div className='app-white__task__left'>
-                            <input type='checkbox'/>
-                            <p>Get my mac fixed</p>
-                        </div>
 
-                        <div className='app-white__task__right'>
-                            <img src={taskSettingImg} className='task-setting' alt=''/>
+                    <div className='app-white__task-container'>
+                        {/* task component - start */}
+                        <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
                         </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                        <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>This is the second, i dont even know what to write bro</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                                            <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                                            <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                                            <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                                            <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
+
+                        {/* task component - start */}
+                                            <div className='app-white__task'>
+                            <div className='app-white__task__left'>
+                                <input type='checkbox'/>
+                                <p className='strikethrough'>Get my mac fixed</p>
+                            </div>
+                            <div className='app-white__task__right'>
+                                <img src={taskSettingImg} className='task-setting' alt=''/>
+                            </div>
+                        </div>
+                        {/* task component - finish */}
                     </div>
                 </div>
             </div>
